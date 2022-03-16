85506edb29e973870dacfd7f6ec93a46ca218ab7
William Scholes <will676work@gmail.com>
2022-03-16 03:36:10 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -135,7 +136,9 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    Billable = "true"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
