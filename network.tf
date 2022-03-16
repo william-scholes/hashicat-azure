2ba4218157e51a8e3a8105e6fa8ac40a82735342
William Scholes <will676work@gmail.com>
2022-03-16 03:50:36 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "example" {
 
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = "rg_name_varThing"
+  resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -26,4 +26,13 @@ module "network" {
   }
 
   depends_on = [azurerm_resource_group.example]
+}
+
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/Test-Org-williamscholes/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = "RG_"
 }
